ca44bf06e3696c62fca2361635012b972ed820fe
Jonathan Kang <jonathankang@gnome.org>
2018-06-04 21:55:25 +0800

    Add UI template to GwWindow
    data/gw-window.ui is added as a template to populate the whole window.

--- src/gw-window.c
+++ src/gw-window.c
@@ -31,11 +31,16 @@ G_DEFINE_TYPE (GwWindow, gw_window, GTK_TYPE_APPLICATION_WINDOW)
 static void
 gw_window_init (GwWindow *window)
 {
+    gtk_widget_init_template (GTK_WIDGET (window));
 }
 
 static void
 gw_window_class_init (GwWindowClass *klass)
 {
+    GtkWidgetClass *widget_class = GTK_WIDGET_CLASS (klass);
+
+    gtk_widget_class_set_template_from_resource (widget_class,
+                                                 "/org/gnome/Weibo/gw-window.ui");
 }
 
 GtkWidget *
