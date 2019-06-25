7c2b1f3b9773fbf86c603eb8e79321ddb66637e5
Jonathan Kang <jonathankang@gnome.org>
2019-06-25 10:25:47 +0800

    name-button: Include gtk/gtk.h

--- src/wb-name-button.c
+++ src/wb-name-button.c
@@ -15,7 +15,10 @@
  *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-	#include "wb-name-button.h"
+
+#include <gtk/gtk.h>
+
+#include "wb-name-button.h"
 
 struct _WbNameButton
 {
